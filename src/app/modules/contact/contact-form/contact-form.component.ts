1c018965f3163482af094fa670e2adc4935744ba
netomedeirosdev <netomedeirosdev@gmail.com>
2023-12-01 02:32:34 -0300

    feat: contact form component

--- src/app/modules/contact/contact-form/contact-form.component.ts
+++ src/app/modules/contact/contact-form/contact-form.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
-  selector: 'app-contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: []
+    selector: 'app-contact-form',
+    templateUrl: './contact-form.component.html',
+    styleUrls: [],
 })
 export class ContactFormComponent {
 
+    public contactForm = this.formBuilder.group({
+        name: [ '', [Validators.minLength(3), Validators.maxLength(50)] ],
+        phoneNumber: [ '', Validators.required ],
+        subject: [ '', [Validators.minLength(10), Validators.maxLength(50)] ],
+        message: [ '', [Validators.minLength(50), Validators.maxLength(500)] ]
+    });
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private messageService: MessageService
+    ) {}
+
+    public handleSubmitContactForm() {
+        if (this.contactForm.valid) {
+            this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Menssagem enviada com sucesso, entraremos em contato em breve!',
+                life: 6000
+            })
+            this.contactForm.reset()
+        }
+    }
 }
--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -1,23 +1,44 @@
-import { NgModule } from '@angular/core';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TooltipModule } from 'primeng/tooltip';
 import { CommonModule } from '@angular/common';
-import { ContactHomeComponent } from './page/contact-home/contact-home.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { SharedModule } from '../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { SharedModule } from '../shared/shared.module';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CONTACT_ROUTES } from './contact.routing';
+import { ContactHomeComponent } from './page/contact-home/contact-home.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormControlAlertComponent } from './form-control-alert/form-control-alert.component';
 
 
 
 @NgModule({
   declarations: [
     ContactHomeComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    FormControlAlertComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(CONTACT_ROUTES),
+    FormsModule,
+    ReactiveFormsModule,
 
-    SharedModule
+    SharedModule,
+
+    AnimateOnScrollModule,
+    CardModule,
+    InputTextModule,
+    InputMaskModule,
+    InputTextareaModule,
+    ButtonModule,
+    TooltipModule,
+
   ]
 })
 export class ContactModule { }
