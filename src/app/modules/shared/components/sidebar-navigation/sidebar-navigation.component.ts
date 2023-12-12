4a95fa9a242e64653c40b4b14b8b25b79896e104
netomedeirosdev <netomedeirosdev@gmail.com>
2023-12-12 01:53:31 -0300

    Feat iterations with logged in state

--- src/app/modules/shared/components/sidebar-navigation/sidebar-navigation.component.ts
+++ src/app/modules/shared/components/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { UserService } from './../../../../services/user/user.service';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {
     IconDefinition,
@@ -9,13 +10,15 @@ import {
     faRightToBracket,
     faUserPlus
 } from '@fortawesome/free-solid-svg-icons';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-sidebar-navigation',
     templateUrl: './sidebar-navigation.component.html',
     styleUrls: [],
 })
-export class SidebarNavigationComponent {
+export class SidebarNavigationComponent implements OnInit {
+
     public readonly faSiginIcon: IconDefinition = faRightToBracket;
     public readonly faSigupIcon: IconDefinition = faUserPlus;
     public readonly faSideBarIcon: IconDefinition = faBars;
@@ -25,8 +28,15 @@ export class SidebarNavigationComponent {
 
     public sidebarVisible: boolean = false;
 
+    public $isLoggedIn!: Subject<boolean>;
+
     public constructor(
         private router: Router,
+        private userService: UserService
     ) { }
+
+    public ngOnInit(): void {
+        this.$isLoggedIn.next(this.userService.isLoggedIn());
+    }
 
 }
